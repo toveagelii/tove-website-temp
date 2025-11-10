dca775f545123cbe4d75409d084a11e03461feb7
Tove Agelii <toveagelii@gmail.com>
2025-11-10 16:32:06 -0500

    Update to images.remotePatterns to remove deprecation warning

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["cdn.sanity.io"], // allow Next.js to load images from Sanity
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
   },
 };
 
